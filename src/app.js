4577c07b7c13535935fef9d7a00a0fb0d17cae88
Anna Reyes <avreyes703@outlook.com>
2024-02-02 21:13:13 -0500

    made changes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const pastesRouter = require("./pastes/pastes.router")
+const usersRouter = require("./users/users.router");
 
 // TODO: Follow instructions in the checkpoint to implement ths API.
 const pastes = require("./data/pastes-data");
 app.use(express.json())
 
 
+app.use("/users", usersRouter);
 app.use("/pastes", pastesRouter) // Note:app.use
 
 
--- src/pastes/pastes.controller.js
+++ src/pastes/pastes.controller.js
@@ -1,7 +1,8 @@
 const pastes = require("../data/pastes-data")
 
 function list(req, res) {
-    res.json({ data: pastes })
+    const {userId} = req.params
+    res.json({ data: pastes.filter(userId ? paste => paste.user_id == userId : () => true) } )
 }
 
 let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0)
@@ -69,6 +70,7 @@ function bodyDataHas(propertyName) {
         const { pasteId } = req.params;
         const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
         if (foundPaste) {
+            res.locals.paste = foundPaste
           return next();
         }
         next({
@@ -78,24 +80,21 @@ function bodyDataHas(propertyName) {
       }
       
       function read(req, res) {
-        const { pasteId } = req.params;
-        const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
-        res.json({ data: foundPaste });
+        res.json({ data: res.locals.paste });
       }
 
       function update(req, res) {
-        const { pasteId } = req.params;
-        const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
+        const paste = res.locals.paste
         const { data: { name, syntax, expiration, exposure, text } = {} } = req.body;
       
         // Update the paste
-        foundPaste.name = name;
-        foundPaste.syntax = syntax;
-        foundPaste.expiration = expiration;
-        foundPaste.exposure = exposure;
-        foundPaste.text = text;
+        paste.name = name;
+        paste.syntax = syntax;
+        paste.expiration = expiration;
+        paste.exposure = exposure;
+        paste.text = text;
       
-        res.json({ data: foundPaste });
+        res.json({ data: paste });
       }
 
       function destroy(req, res) {
